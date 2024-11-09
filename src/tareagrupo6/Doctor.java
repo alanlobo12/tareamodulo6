ddc2fd2d94d839d05d18cd32620a24b595a09c86
alanlobo12 <alanlobo153@gmail.com>
2024-11-08 20:17:59 -0600

    Commit prueba de rama

--- src/tareagrupo6/Doctor.java
+++ src/tareagrupo6/Doctor.java
@@ -2,5 +2,20 @@
 package tareagrupo6;
 
 public class Doctor {
-    
+     private String especialidad;
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public void mostrarInformacionDoctor() {
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Número de ID: " + getNumId());
+        System.out.println("Edad: " + getEdad() + " años");
+        System.out.println("Especialidad: " + especialidad);
+    }
 }
